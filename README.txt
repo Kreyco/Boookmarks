1547ed1e4e4c281e1cdbde8becd885bde9321107
BinLa <wmarin@tiqal.com>
2018-02-17 17:00:19 -0500

    First commit

---
+++ README.txt
@@ -0,0 +1,10 @@
+App developed with AngularJS (Angular 1)
+Tutorial getting from: https://quizzpot.com/courses/introduccion-a-angular-js
+Versions: 
+-AngularJS: https://ajax.googleapis.com/ajax/libs/angularjs/1.3.0-rc.1/angular.min.js
+-Bootstrap: https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css
+	    https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js
+-JQuery: https://code.jquery.com/jquery-2.1.1.min.js
+-GIT: 
+
+This app is a bookmark controller. Adding, sorting and deleting bookmarks, using AngularJS, Bootstrap and GIT as a repository.
---
+++ js/app.js
@@ -0,0 +1,34 @@
+( function () {
+    "use strict"
+
+    var  app = angular.module('Bookmarks',[
+        //Dependencies here
+    ])
+
+    .controller( 'MainController', function( $scope ) {
+        //Example app
+        $scope.name = 'Jhon Doe';
+
+        //Bookmarks app
+        $scope.userName = 'Will';
+        $scope.categories = [ 'HTML','JavaScript','CSS', 'Games' ];
+
+        $scope.bookmarks = [ //<--- Adding the data
+            {id:1,name:'Quizzpot.com',url:'https://quizzpot.com',category:'JavaScript'},
+            {id:2,name:'Html5 Game Devs',url:'https://html5gamedevs.com',category:'Games'},
+            {id:3,name:'CSS Tricks',url:'http://css-tricks.com',category:'CSS'},
+            {id:4,name:'Bootstrap',url:'http://getbootstrap.com',category:'CSS'},
+            {id:5,name:'Card',url:'http://jessepollak.github.io/card/',category:'JavaScript'}
+        ];
+
+        $scope.currentCategory = undefined;
+
+        $scope.setCurrentCategory = function( category ) {
+            $scope.currentCategory = category;
+        }
+        $scope.isCurrentCategory = function( category ) {
+            return $scope.currentCategory === category;
+        }
+    });
+    console.log( app );
+})();
