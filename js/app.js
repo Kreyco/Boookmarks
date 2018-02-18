b4bdeb4e006092a9672db4cf935669cebc7952e5
kreyco <wilman.marin01@unicatolica.edu.co>
2018-02-17 20:04:27 -0500

    CRUD working with an array

--- js/app.js
+++ js/app.js
@@ -14,11 +14,11 @@
         $scope.categories = [ 'HTML','JavaScript','CSS', 'Games' ];
 
         $scope.bookmarks = [ //<--- Adding the data
-            {id:1,name:'Quizzpot.com',url:'https://quizzpot.com',category:'JavaScript'},
-            {id:2,name:'Html5 Game Devs',url:'https://html5gamedevs.com',category:'Games'},
-            {id:3,name:'CSS Tricks',url:'http://css-tricks.com',category:'CSS'},
-            {id:4,name:'Bootstrap',url:'http://getbootstrap.com',category:'CSS'},
-            {id:5,name:'Card',url:'http://jessepollak.github.io/card/',category:'JavaScript'}
+            {id:1,title:'Quizzpot.com',url:'https://quizzpot.com',category:'JavaScript'},
+            {id:2,title:'Html5 Game Devs',url:'https://html5gamedevs.com',category:'Games'},
+            {id:3,title:'CSS Tricks',url:'http://css-tricks.com',category:'CSS'},
+            {id:4,title:'Bootstrap',url:'http://getbootstrap.com',category:'CSS'},
+            {id:5,title:'Card',url:'http://jessepollak.github.io/card/',category:'JavaScript'}
         ];
 
         $scope.currentCategory = undefined;
@@ -29,8 +29,32 @@
         $scope.isCurrentCategory = function( category ) {
             return $scope.currentCategory === category;
         };
+        $scope.showWindow = function(bookmark){ // Step 1
+            $scope.bookmarkForm.$setPristine(); //Step 2
+            $scope.bookmarkForm.$setUntouched();
+
+            bookmark = bookmark || {category:$scope.currentCategory,url:''}; //Step 3
+            $scope.bookmark = bookmark;
+            $('#bookmarkModal').modal('show');  //Step 4
+        };
         $scope.save = function(bookmark){
-            console.log(bookmark);
+            if($scope.bookmarkForm.$valid){                //Step 1
+                if(!bookmark.id){                          //Step 2
+                    var record = angular.copy(bookmark);   //Step 3
+
+                    record.id = $scope.bookmarks.length;   //Step 4
+                    $scope.bookmarks.push(record);         //Step 5
+                }
+                $('#bookmarkModal').modal('hide');         //Step 6
+            }
+        };
+        $scope.remove = function(id){                //Step 1
+            for(var i=0,len=$scope.bookmarks.length;i<len;i++){ //Step 2
+                if($scope.bookmarks[i].id === id){   //Step 3
+                    $scope.bookmarks.splice(i,1);    //Step 4
+                    break;
+                }
+            }
         };
     });
     console.log( app );
